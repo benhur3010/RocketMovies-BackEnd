ec74cc9f65b89f900cd1c6e096d4350ab5be28b1
Ben-Hur <benhurbueno.adv@gmail.com>
2022-12-15 18:40:39 -0300

    first commit

---
+++ src/routes/users.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+
+const UsersController = require("../controllers/UsersController"); // Tem que ser importado do arquivo UsersController para instanciar aqui.
+
+const usersRoutes = Router();
+
+const usersController = new UsersController(); // Como ele é uma classe, é necessário instanciar, ou seja, alocar um espaço na memória para a classe.
+
+usersRoutes.post("/", usersController.create); // Acessa o método "create" que foi criado no arquivo UsersController.
+usersRoutes.put("/:id", usersController.update);
+
+module.exports = usersRoutes;
---
+++ src/server.js
@@ -0,0 +1,41 @@
+require('express-async-errors'); // Para lidar com os erros.
+
+const migrationsRun = require('./database/sqlite/migrations');
+
+const AppError = require('./utils/AppError');
+
+const express = require('express'); // Criar uma variável que recolhe todas as dependências do express.
+
+const routes = require('./routes'); // Importando as rotas do arquivo index de "routes". 
+
+migrationsRun(); // Para executar o banco de dados.
+
+const app = express(); // Função que está inicializando o express.
+
+app.use(express.json()); // Serve para saber que as requisições serão feitas no padrão json.
+
+app.use(routes); // Executa as rotas.
+
+// Request é receber uma requisição e o response é a resposta fornecida.
+
+app.use((error, request, response, next) => {
+  
+  // Se o erro for do lado do cliente:
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  });
+});
+
+const PORT = 3333; // Variável que armazena o endereço que o express irá atender.
+
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); // Irá rodar o servidor.
